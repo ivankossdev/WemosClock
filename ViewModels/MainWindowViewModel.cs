918a4a7e33df935ff821089e3717d1dee870f81a
ivan.koss <ivan.koss@yandex.ru>
2026-03-24 23:21:00 +0300

    show TextBox string

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,24 @@
-﻿namespace WemosClock.ViewModels;
+﻿using System;
+
+namespace WemosClock.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Поиск устройств";
+    private string _resultText = "Результат\n";
+
+    public string ResultText
+    {
+        get => _resultText;
+        set
+        {
+            _resultText = value;
+            OnPropertyChanged();
+        }
+    }
 
     public void Search()
     {
-        System.Console.WriteLine("Press button");
+        ResultText += "Найдено устройство: " + DateTime.Now + "\n";
     }
 }
