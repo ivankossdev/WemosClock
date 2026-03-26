c8041152120c04cf186eb1550e6688a2955a5cb7
ivan.koss <ivan.koss@yandex.ru>
2026-03-26 22:30:38 +0300

    async Task Search()

--- ViewModels/SearchViewModel.cs
+++ ViewModels/SearchViewModel.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
 using System;
 
 namespace WemosClock.ViewModels
@@ -9,10 +10,23 @@ namespace WemosClock.ViewModels
         [ObservableProperty]
         private string _newResultText  = "Результат\n";
 
+        private int count = 0; 
+
         [RelayCommand]
-        private void Search()
+        private async Task Search()
         {
-            NewResultText += "Найдено устройство: " + DateTime.Now + "\n";
+            NewResultText = "Результат\n";
+
+            Random rnd = new();
+            for (int i = 0; i < rnd.Next(2, 50); i++)
+            {
+                await Task.Delay(rnd.Next(100, 1500)); // имитация долгой операции
+                NewResultText += $"Найдено устройство: {count += 1}\t" + DateTime.Now + "\n";
+            }
+
+            NewResultText += "Поиск завершен.\n";
+            count = 0;
         }
+        
     }
 }
