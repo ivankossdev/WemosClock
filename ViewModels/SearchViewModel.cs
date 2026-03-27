275c57dc9b3cf124026076749f1bff567e267bd9
ivankossdev <ivan.koss@yandex.ru>
2026-03-27 08:52:32 +0300

    ref code

--- ViewModels/SearchViewModel.cs
+++ ViewModels/SearchViewModel.cs
@@ -8,20 +8,21 @@ namespace WemosClock.ViewModels
     public partial class SearchViewModel : ObservableObject
     {
         [ObservableProperty]
-        private string _newResultText  = "Результат\n";
+        private string _newResultText  = string.Empty;
 
         private int count = 0; 
 
         [RelayCommand]
         private async Task Search()
         {
-            NewResultText = "Результат\n";
+            NewResultText = string.Empty;
 
             Random rnd = new();
             for (int i = 0; i < rnd.Next(2, 50); i++)
             {
+                if(i == 0) NewResultText += "Найдено:\n";
                 await Task.Delay(rnd.Next(100, 1500)); // имитация долгой операции
-                NewResultText += $"Найдено устройство: {count += 1}\t" + DateTime.Now + "\n";
+                NewResultText += $"Устройство: {count += 1}\t" + DateTime.Now + "\n";
             }
 
             NewResultText += "Поиск завершен.\n";
