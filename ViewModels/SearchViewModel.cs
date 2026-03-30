02faac4de10b439b6630c00c0db0ef0519e54463
ivankossdev <ivan.koss@yandex.ru>
2026-03-30 13:39:27 +0300

    isSearch status

--- ViewModels/SearchViewModel.cs
+++ ViewModels/SearchViewModel.cs
@@ -9,30 +9,34 @@ namespace WemosClock.ViewModels
     {
         [ObservableProperty]
         private string _newResultText  = string.Empty;
-
         private int count = 0; 
+        private bool isSearch = false; 
 
         [RelayCommand]
         private async Task Search()
         {
             NewResultText = string.Empty;
+            isSearch = true; 
 
             Random rnd = new();
             for (int i = 0; i < rnd.Next(2, 50); i++)
             {
                 if(i == 0) NewResultText += "Найдено:\n";
                 await Task.Delay(rnd.Next(100, 1500)); // имитация долгой операции
-                NewResultText += $"Устройство: {count += 1}\t" + DateTime.Now + "\n";
+                NewResultText += $"[ {count += 1} ]\t" + DateTime.Now + "\n";
             }
 
             NewResultText += "Поиск завершен.\n";
             count = 0;
+
+            isSearch = false;
         }
         
         [RelayCommand]
         private void Clear()
         {
-            NewResultText = string.Empty;
+            if(!isSearch)
+                NewResultText = string.Empty;
         }
     }
 }
